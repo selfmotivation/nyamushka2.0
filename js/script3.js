c70f9bd26e238c3a5365f0ec5656f04c4a03e9c0
Andrey Kiselev <mira55@yandex.ru>
2022-06-18 22:26:07 +0400

    refactor: fix some code

--- js/script3.js
+++ js/script3.js
@@ -1,121 +1,125 @@
-let cardsData;
-(async function getCardsData() {
-  await fetch(`../cards-data.json`)
-  .then(res => res.json())
-  .then(data => cardsData = data);
-  console.log(cardsData);
-}()
-.then(() => {
-  const createCard = (cardData) => {
-    const card = `
-      <div class="food-item${cardData.isOver ? ' is_over' : ''}">
+const getCardsData = async(url) => {
+  const res = await fetch(url);
+  const data = await res.json();
+  
+  return data;
+};
+
+const createCards = (cardData) => {
+  const cards = cardData.
+    map(card => {
+    return `<div class="food-item${card.isOver ? ' is_over' : ''}">
       <div class="food-card default">
         <div class="food-card__text">
           <p class="food-card__description">Сказочное заморское яство</p>
           <p class="food-card__title">Нямушка</p>
-          <p class="food-card__taste">${cardData.taste}</p>
-          <p class="food-card__contains"><span class="food-card__contains-quantity">${cardData.portionsQuantity} </span>порций<br>${cardData.mice} в подарок${cardData.isCustomerSatisfied ? '<br>заказчик доволен' : ''}</p>
+          <p class="food-card__taste">${card.taste}</p>
+          <p class="food-card__contains"><span class="food-card__contains-quantity">${card.portionsQuantity} </span>порций<br>${card.mice} в подарок${card.isCustomerSatisfied ? '<br>заказчик доволен' : ''}</p>
         </div>
           <img class="food-card__cat-bg" src="img/cat-bg.png">
-          <div class="food-card__weight">${cardData.weight}<br><span class="food-card__weight-kg">кг</span></div>
-        </div>
+          <div class="food-card__weight">${card.weight}<br><span class="food-card__weight-kg">кг</span></div>
+      </div>
       <p class="food-item__subscription">
-        ${cardData.isOver ?
-          'Печалька, '+cardData.taste+' закончился.' :
+        ${card.isOver ?
+          'Печалька, '+card.taste+' закончился.' :
           'Чего сидишь? Порадуй котэ, <span class="food-item__subscription-buy">купи</span>.</p>'
         }
     </div>`
-  
-    return card;
-  }
-  
-  
-  const createAllCards = () => {
-    const cardsList = [];
+    }).join('');
     
-    cardsData.forEach(card => {
-      cardsList.push(createCard(card));
-    });
-  
-    document.querySelector('.food-promo').innerHTML += cardsList.join('');
-  }
-  
-  createAllCards();
-  
-  
-  const foodCards = document.querySelectorAll('.food-card');
-  
-  foodCards.forEach(card => {
-    if (card.parentElement.classList.contains('is_over')) {
-      card.nextElementSibling.innerHTML = `Печалька, ${card.querySelector('.food-card__taste').innerHTML} закончился.`;
-    } else {
-      card.addEventListener('mouseenter', addHoverEffect);
-      card.addEventListener('mouseleave', removeHoverEffect);
-      card.addEventListener('click', changeCardOrderState);
-    }
+    document.querySelector('.food-promo').innerHTML += cards;
+
+    const buySubscriptionButton = document.querySelectorAll('.food-item__subscription-buy');
+    buySubscriptionButton.forEach(btn => {
+    btn.addEventListener('click', () => changeCardColorByText(btn));
+    btn.addEventListener('click', () => changeSubscriptionByText(btn, cardData));
   });
+}
+
+const addHoverEffect = (card) => {
+  if (card.classList.contains('chosen')) {
+    const cardDescription = card.querySelector('.food-card__description');
+    cardDescription.innerHTML = "Котэ не одобряет?";
+    cardDescription.classList.add('unchose');
+  }
+
+  card.classList.add('hover');
+}
+
+const removeHoverEffect = (card) => {
+  const cardDescription = card.querySelector('.food-card__description');
+  cardDescription.innerHTML = 'Сказочное заморское яство';
+  cardDescription.classList.remove('unchose');
   
-  function addHoverEffect() {
-    if (this.classList.contains('chosen')) {
-      const cardDescription = this.querySelector('.food-card__description');
-      cardDescription.innerHTML = "Котэ не одобряет?";
-      cardDescription.classList.add('unchose');
-    }
+  card.classList.remove('hover');
+}
 
-    this.classList.add('hover');
-  }
-
-  function removeHoverEffect() {
-    const cardDescription = this.querySelector('.food-card__description');
-    cardDescription.innerHTML = 'Сказочное заморское яство';
+const changeCardOrderState = (card, cardsData, e) => {
+  if (e.currentTarget.classList.contains('food-card')) {
+    const cardDescription = card.querySelector('.food-card__description');
     cardDescription.classList.remove('unchose');
     
-    this.classList.remove('hover');
-  }
-
-  function changeCardOrderState() {
-    const cardDescription = this.querySelector('.food-card__description');
-    cardDescription.classList.remove('unchose');
-    
-    if (this.classList.contains('chosen')) {
+    if (card.classList.contains('chosen')) {
       cardDescription.innerHTML = 'Сказочное заморское яство';
     }
 
-    this.classList.toggle('chosen');
-    this.classList.remove('hover');
+    card.classList.toggle('chosen');
+    card.classList.remove('hover');
 
-    const taste = this.querySelector('.food-card__taste').innerHTML;
-    const subscription = this.nextElementSibling;
+    const taste = card.querySelector('.food-card__taste').innerHTML;
+    const subscription = card.nextElementSibling;
     
-    if (this.classList.contains('chosen')) {
+    if (card.classList.contains('chosen')) {
       cardsData.forEach(cardData => {
         if (cardData.taste === taste) subscription.innerHTML = `${cardData.descriptionIfOrdered}`;
       });
     } else {
-      subscription.innerHTML = 'Чего сидишь? Порадуй котэ, <span>купи</span>.';
-      subscription.querySelector('span').classList.add('food-item__subscription-buy');
-      subscription.querySelector('.food-item__subscription-buy').addEventListener('click', changeCardColorByText);
-      subscription.querySelector('.food-item__subscription-buy').addEventListener('click', changeSubscriptionByText);
+      subscription.innerHTML = 'Чего сидишь? Порадуй котэ, <span class="food-item__subscription-buy">купи</span>.';
+      const buyTextButton = subscription.querySelector('.food-item__subscription-buy');
+      buyTextButton.addEventListener('click', () => changeCardColorByText(buyTextButton));
+      buyTextButton.addEventListener('click', () => changeSubscriptionByText(buyTextButton, cardsData));
     }
   }
 
-  function changeCardColorByText() {
-    this.parentElement.previousElementSibling.classList.toggle('chosen');
+  if (e.currentTarget.classList.contains('food-item__subscription-buy')) {
+    changeCardColorByText(e.currentTarget);
   }
+}
+
+const changeCardColorByText = (buyTextButton) => {
+  buyTextButton.parentElement.previousElementSibling.classList.toggle('chosen');
+}
+
+const changeSubscriptionByText = (buyTextButton, cardsData) => {
+  const taste = buyTextButton.parentElement.previousElementSibling.querySelector('.food-card__taste').innerHTML;
+  const subscription = buyTextButton.parentElement;
+  if (subscription.previousElementSibling.classList.contains('chosen')) {
+    cardsData.forEach(cardData => {
+      if (cardData.taste === taste) subscription.innerHTML = `${cardData.descriptionIfOrdered}`;
+    });
+  }
+}
+
+const checkIfOutOfStock = (cards, cardsData) => {
+  cards.forEach(card => {
+    if (card.parentElement.classList.contains('is_over')) {
+      card.nextElementSibling.innerHTML = `Печалька, ${card.querySelector('.food-card__taste').innerHTML} закончился.`;
+    } else {
+      card.addEventListener('mouseenter', () => addHoverEffect(card));
+      card.addEventListener('mouseleave', () => removeHoverEffect(card));
+      card.addEventListener('click', (e) => changeCardOrderState(card, cardsData, e));
+    }
+  });
+}
+
+
+
+document.addEventListener('DOMContentLoaded', async () => {
+  const cardsData = await getCardsData(`../cards-data.json`);
   
-  function changeSubscriptionByText() {
-    const taste = this.parentElement.previousElementSibling.querySelector('.food-card__taste').innerHTML;
-    const subscription = this.parentElement;
-    if (subscription.previousElementSibling.classList.contains('chosen')) {
-      cardsData.forEach(cardData => {
-        if (cardData.taste === taste) subscription.innerHTML = `${cardData.descriptionIfOrdered}`;
-      });
-    }
-  }
+  createCards(cardsData);
 
-  const buySubscriptionButton = document.querySelectorAll('.food-item__subscription-buy');
-  buySubscriptionButton.forEach(function(btn){
-    btn.addEventListener('click', changeCardColorByText);
-    btn.addEventListener('click', changeSubscriptionByText);
-  })
-}));
+  const foodCards = document.querySelectorAll('.food-card');
+  
+  checkIfOutOfStock(foodCards, cardsData);
+})
